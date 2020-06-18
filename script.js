7a896ee7e85341937d08e51697bd334ad8005fc6
Mentalgrudgeness101 <67037408+Mentalgrudgeness101@users.noreply.github.com>
2020-06-17 18:02:27 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+
+
+function playAudio() {
+  if (isPlaying) {
+    document.getElementById("myAudio").pause();
+    isPlaying = false;
+  } else {
+    document.getElementById("myAudio").play();
+    isPlaying = true;
+  }
+    document.getElementById("myButton").classList.add("buttonMove");
+}
+
+function scrollDown() {
+    document.getElementById("scrollDown").classList.add("fadeMouse");
+    
+}
+
+function contact() {
+    prompt("Please tell us your name","name");
+    prompt("Enter an email we can contact you at","email");
+}
+		
+
